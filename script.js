42412b9a1c3987ab4d03d06b4c6f5e3ac6403d9b
AlexY <alexyo87@yahoo.com>
2019-02-15 12:27:07 +0300

    project start

---
+++ script.js
@@ -0,0 +1,38 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.querySelector("button");
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function get2RandomColors() {
+	color1.value = getRandomColor();
+	color2.value = getRandomColor();
+	setGradient();
+
+}
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", get2RandomColors);  
